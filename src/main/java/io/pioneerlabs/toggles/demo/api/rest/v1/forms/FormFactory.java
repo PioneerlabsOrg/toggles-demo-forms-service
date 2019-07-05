a781aa5ee5d8905685922dba9cca46920df8b8f6
pioneerlans <filippo@pioneerlabs.io>
2019-07-05 08:45:25 +0100

    @F refactored FormFactory

--- src/main/java/io/pioneerlabs/toggles/demo/api/rest/v1/forms/FormFactory.java
+++ src/main/java/io/pioneerlabs/toggles/demo/api/rest/v1/forms/FormFactory.java
@@ -18,23 +18,15 @@ public class FormFactory {
     private final FormControllerV1 formControllerV1;
     private final FormControllerV2 formControllerV2;
     private final FormControllerV3 formControllerV3;
-    private final FormControllerV4 formControllerV4;
-    private final FormControllerV5 formControllerV5;
-    private final FormControllerV6 formControllerV6;
-    private final FormControllerV7 formControllerV7;
 
-    private BusinessObject businessObject;
 
-    public FormFactory(TogglesClient togglesClient, FormControllerV0 formControllerV0, FormControllerV1 formControllerV1, FormControllerV2 formControllerV2, FormControllerV3 formControllerV3, FormControllerV4 formControllerV4, FormControllerV5 formControllerV5, FormControllerV6 formControllerV6, FormControllerV7 formControllerV7) {
+
+    public FormFactory(TogglesClient togglesClient, FormControllerV0 formControllerV0, FormControllerV1 formControllerV1, FormControllerV2 formControllerV2, FormControllerV3 formControllerV3) {
         this.togglesClient = togglesClient;
         this.formControllerV0 = formControllerV0;
         this.formControllerV1 = formControllerV1;
         this.formControllerV2 = formControllerV2;
         this.formControllerV3 = formControllerV3;
-        this.formControllerV4 = formControllerV4;
-        this.formControllerV5 = formControllerV5;
-        this.formControllerV6 = formControllerV6;
-        this.formControllerV7 = formControllerV7;
     }
 
 
@@ -43,25 +35,17 @@ public class FormFactory {
     public JSONObject createForm(BusinessObject businessObject) {
         log.info("calling createForm()");
 
-        boolean isPostCodeLookUpEnabled = togglesClient.isFeatureOn("postcode-lookup", this.businessObject);
-        boolean isDocUploadEnabled = togglesClient.isFeatureOn("doc-upload", this.businessObject);
-        boolean isQualityCheckEnabled = togglesClient.isFeatureOn("quality-check", this.businessObject);
+        boolean isPostCodeLookUpEnabled = togglesClient.isFeatureOn("postcode-lookup", businessObject);
+        boolean isDocUploadEnabled = togglesClient.isFeatureOn("doc-upload", businessObject);
+        boolean isQualityCheckEnabled = togglesClient.isFeatureOn("quality-check", businessObject);
 
 
-        if(!isPostCodeLookUpEnabled && isDocUploadEnabled && !isQualityCheckEnabled) {
+        if(isPostCodeLookUpEnabled) {
             return formControllerV1.createJsonForm();
-        } else if(!isPostCodeLookUpEnabled && !isDocUploadEnabled && isQualityCheckEnabled) {
+        } else if(isDocUploadEnabled) {
             return formControllerV2.createJsonForm();
-        } else if(!isPostCodeLookUpEnabled && isDocUploadEnabled && isQualityCheckEnabled) {
+        } else if(isQualityCheckEnabled) {
             return formControllerV3.createJsonForm();
-        } else if(isPostCodeLookUpEnabled && isDocUploadEnabled && isQualityCheckEnabled) {
-            return formControllerV4.createJsonForm();
-        } else if(isPostCodeLookUpEnabled && isDocUploadEnabled && !isQualityCheckEnabled) {
-            return formControllerV5.createJsonForm();
-        } else if(isPostCodeLookUpEnabled && !isDocUploadEnabled && isQualityCheckEnabled) {
-            return formControllerV6.createJsonForm();
-        } else if(isPostCodeLookUpEnabled && !isDocUploadEnabled && !isQualityCheckEnabled) {
-            return formControllerV7.createJsonForm();
         } else {
             return formControllerV0.createJsonForm();
         }
